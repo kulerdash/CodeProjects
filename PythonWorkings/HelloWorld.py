e8d948a3888d8d174537b14e64f936580c36a195
kulerdash <thmei0406@icloud.com>
2020-09-08 11:18:43 +0800

    Hello World!

---
+++ PythonWorkings/HelloWorld.py
@@ -0,0 +1,12 @@
+#encoding:utf-8
+'''
+@File    :   HelloWorld.py
+@Time    :   2020/09/08 11:17:42
+@Author  :   kulerdash 
+@Version :   1.0
+@Contact :   thmei0406@icloud.com
+@WebSite :   github.com/kulerdash
+'''
+# Start typing your code from here
+
+print("Hello, 石昊海!")
